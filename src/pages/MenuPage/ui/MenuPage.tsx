230c961bc3f8a5a4c238d9b1385696658069dbc7
AnaninAN <ananinpgu@mail.ru>
2024-11-07 15:53:11 +0300

    delete button

--- src/pages/MenuPage/ui/MenuPage.tsx
+++ src/pages/MenuPage/ui/MenuPage.tsx
@@ -3,16 +3,9 @@ import { memo } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { Page } from '@/widgets/Page';
-import { AppButton } from '@/shared/ui/AppButton';
 
 export const MenuPage = memo(() => {
   const { t } = useTranslation('dish');
 
-  return (
-    <Page>
-      {t('dishes-page')}
-      <br />
-      <AppButton children="Hello!" theme="background_inverted" />
-    </Page>
-  );
+  return <Page>{t('dishes-page')}</Page>;
 });
